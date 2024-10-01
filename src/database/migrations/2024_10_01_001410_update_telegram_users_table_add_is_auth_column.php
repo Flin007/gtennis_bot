9abe8827db409842739058251ac3d40a225d801a
Vladimir <flin007@inbox.ru>
2024-10-01 03:21:16 +0300

    telegram_users table add column is_auth migration

--- src/database/migrations/2024_10_01_001410_update_telegram_users_table_add_is_auth_column.php
+++ src/database/migrations/2024_10_01_001410_update_telegram_users_table_add_is_auth_column.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('telegram_users', function (Blueprint $table){
-            $table->addColumn('boolean', 'is_auth')->after('id');
+            $table->addColumn('boolean', 'is_auth')->after('id')->default(0);
         });
     }
 
